844d2809d8ee1b0f454e5c0967bab6b70036ff7b
Tomoki Morita <tmsongbooks215@gmail.com>
2023-04-01 18:38:29 +0900

    use es module for tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type import("tailwindcss").Config */
-module.exports = {
+export default {
   content: [
     "./resources/views/**/*.blade.php",
     "./storage/framework/views/*.php",
@@ -15,9 +15,8 @@ module.exports = {
       md: "768px",
     },
     fontSize: {
-      xxs: ["0.675rem"],
-      xs: ["0.8125rem"],
-      sm: ["0.9375rem", "1.25rem"],
+      xs: ["0.8125rem", "1.25rem"],
+      sm: ["0.9375rem", "1.5rem"],
       base: ["1.0625rem", "1.85rem"],
       "3xl": ["1.65rem", "2.5rem"],
       "4xl": ["2rem", "2.5rem"],
