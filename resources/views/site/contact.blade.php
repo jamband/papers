c7093b2948e24c50f326d310e5b8e74ff299c8da
Tomoki Morita <tmsongbooks215@gmail.com>
2024-02-21 21:34:41 +0900

    rename twitter to x

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -2,7 +2,7 @@
   <h1>Contact</h1>
   Please to the message via
   <x-link.external href="https://twitter.com/livejam_db">
-    Twitter<x-icon.external-link class="w-4 h-4 align-[-0.125em]" />
+    X<x-icon.external-link class="w-4 h-4 align-[-0.125em]" />
   </x-link.external>
   or
   <x-link.external href="https://github.com/jamband/papers/issues">
