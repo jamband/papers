5054a7e9c5a5ab7f479e9a91de266bf731a08c7a
Tomoki Morita <tmsongbooks215@gmail.com>
2024-04-24 10:08:23 +0900

    change from DATABASE_URL to DB_URL

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ return [
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('app.db')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
