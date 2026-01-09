14f7a1dcc9b37c91c4ab58d62d388a553d923cd8
Tomoki Morita <tmsongbooks215@gmail.com>
2026-01-09 09:50:05 +0900

    remove unnecessary parentheses

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -23,9 +23,9 @@ abstract class DuskTestCase extends BaseTestCase
 
     protected function driver(): RemoteWebDriver
     {
-        $options = (new ChromeOptions())->addArguments((new Collection([
+        $options = new ChromeOptions()->addArguments(new Collection([
             $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
-        ]))->unless($this->hasHeadlessDisabled(), function (Collection $items) {
+        ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
             return $items->merge([
                 '--disable-gpu',
                 '--headless',
